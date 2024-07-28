896aac40fbfa3bd032116b2698e0e2e1ce8cd561
wit <witwitchayakarn@gmail.com>
2024-07-28 11:55:49 +0700

    Change N_URLS from 10 to 30

--- src/const.js
+++ src/const.js
@@ -17,7 +17,7 @@ export const EXTRACT_LOG = 'ExtractLog';
 export const EXTRACT = 'Extract';
 export const BRACE_STATIC_FILES = 'brace-001.appspot.com';
 
-export const N_URLS = 10;
+export const N_URLS = 30;
 export const N_EXTRACTS = 3;
 
 export const HTTP = 'http://';
